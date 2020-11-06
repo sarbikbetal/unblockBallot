1177aaa56205f9df5829e12c36748bf67bb9597d
Sarbik Betal <sarbikb55@gmail.com>
2020-11-06 17:05:22 +0530

    Initial commit

---
+++ pages/dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+      <blockquote class="blockquote">
+        &#8220;hello all.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style>
+</style>
