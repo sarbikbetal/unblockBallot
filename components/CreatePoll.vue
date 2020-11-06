724bc207863941ba57b8cd3423cf5532192a8f08
Sarbik Betal <sarbikb55@gmail.com>
2020-11-07 03:00:43 +0530

    Add time/date pickers

--- components/CreatePoll.vue
+++ components/CreatePoll.vue
@@ -7,6 +7,7 @@
       <v-expansion-panel-content>
         <v-form>
           <v-container>
+            <!-- Title/Question -->
             <v-row>
               <v-text-field
                 @keydown.enter="focusAddOptions"
@@ -14,6 +15,7 @@
                 outlined
               ></v-text-field>
             </v-row>
+            <!-- Created options -->
             <v-row v-for="(opt, i) in options" :key="i">
               <v-text-field
                 v-model="options[i].text"
@@ -24,6 +26,7 @@
               ></v-text-field>
             </v-row>
             <v-row>
+              <!-- Create new option -->
               <v-text-field
                 ref="addOptionTextField"
                 v-model="tempOpt"
@@ -36,6 +39,86 @@
                 @keydown.enter="addOption"
                 @click:clear="clearTemp"
               ></v-text-field>
+            </v-row>
+            <v-row>
+              <!-- Time picker -->
+              <v-col cols="11" sm="5">
+                <v-dialog
+                  ref="timeDialog"
+                  v-model="timePicker"
+                  :return-value.sync="time"
+                  persistent
+                  width="290px"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                      v-model="time"
+                      label="Select time"
+                      prepend-icon="mdi-clock-time-four-outline"
+                      readonly
+                      v-bind="attrs"
+                      v-on="on"
+                    ></v-text-field>
+                  </template>
+                  <v-time-picker
+                    ampm-in-title
+                    v-if="timePicker"
+                    v-model="time"
+                    full-width
+                  >
+                    <v-spacer></v-spacer>
+                    <v-btn text color="primary" @click="timePicker = false">
+                      Cancel
+                    </v-btn>
+                    <v-btn
+                      text
+                      color="primary"
+                      @click="$refs.timeDialog.save(time)"
+                    >
+                      OK
+                    </v-btn>
+                  </v-time-picker>
+                </v-dialog>
+              </v-col>
+              <v-spacer />
+              <!-- Date picker -->
+              <v-col cols="11" sm="5">
+                <v-dialog
+                  ref="dateDialog"
+                  v-model="datePicker"
+                  :return-value.sync="date"
+                  persistent
+                  width="290px"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                      v-model="date"
+                      label="Select Date"
+                      prepend-icon="mdi-calendar"
+                      readonly
+                      v-bind="attrs"
+                      v-on="on"
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker
+                    :min="new Date().toISOString().substr(0, 10)"
+                    v-model="date"
+                    scrollable
+                  >
+                    <v-spacer></v-spacer>
+                    <v-btn text color="primary" @click="datePicker = false">
+                      Cancel
+                    </v-btn>
+                    <v-btn
+                      text
+                      color="primary"
+                      @click="$refs.dateDialog.save(date)"
+                    >
+                      OK
+                    </v-btn>
+                  </v-date-picker>
+                </v-dialog>
+              </v-col>
             </v-row>
           </v-container>
         </v-form>
@@ -60,8 +143,11 @@ export default {
       title: "",
       options: [],
       tempOpt: "",
-      closesAt: Date.now() + 3600000,
       snackbar: false,
+      time: null,
+      timePicker: false,
+      date: new Date().toISOString().substr(0, 10),
+      datePicker: false,
     };
   },
   methods: {
