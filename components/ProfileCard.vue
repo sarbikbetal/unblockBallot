972317ab014f96307427f9b9eb4514afe1cee2d4
Sarbik Betal <sarbikb55@gmail.com>
2020-11-06 19:42:11 +0530

    Added profile card

---
+++ components/ProfileCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <v-row justify="space-around">
+      <v-card outlined width="400">
+        <v-card-title class="white--text mt-8 justify-center">
+          <v-avatar size="110">
+            <img
+              alt="user"
+              src="https://avataaars.io/"
+            />
+          </v-avatar>
+        </v-card-title>
+        <v-card-subtitle>
+          <p class="text-h5 text-center">Random Stranger</p>
+        </v-card-subtitle>
+      </v-card>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
